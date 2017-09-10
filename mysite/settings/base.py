3845d94b5951abd9416a334b286fb89981c57be3
Steve Shead <steve@steve-shead.com>
2017-09-10 10:40:00 -0700

    customized admin

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'dashboard',
     'wagtail.wagtailforms',
     'wagtail.wagtailredirects',
     'wagtail.wagtailembeds',
